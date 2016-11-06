2f0b09d3ff9b0d374dc431fc21864979ebfab4cf
Francisco Cabral <franciscocabralfilho@gmail.com>
2016-11-06 18:15:58 -0300

    remoção

--- trabalho final/js-server/routes/reminder.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET reminder listing. */
-router.get('/', function(req, res, next) {
-    res.send({teste:1243});
-});
-
-module.exports = router;
